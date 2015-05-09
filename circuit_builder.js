6ebf47461dd707ee9a20594cf244fdfe1a51a18d
Daniel Pasillas <dpasi001@ucr.edu>
2015-05-08 18:39:01 -0700

    update interval test

--- circuit_builder.js
+++ circuit_builder.js
@@ -1,6 +1,18 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+var x = 100;
+var y = 100;
 
-context.moveTo(0,0);
-context.lineTo(100,100);
-context.stroke();
+function update(context)
+{
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.beginPath();
+	context.moveTo(0,y);
+	context.lineTo(x,0);
+	context.stroke();
+	x++;
+	y++;
+}
+
+
+var timer = setInterval(update,1000/25, context);
